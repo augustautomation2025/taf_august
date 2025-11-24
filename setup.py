d9caab155ba83f6509683be1cd04ed4604069bff
admin <kattubadi426@gmail.com>
2025-11-24 08:15:07 +0530

    Framework code added

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='taf_august',
-    version='1.0.0',
+    version='2.0.0',
     author='Sreenivasulu Kattubadi',
     description='Test Automation Framework for Data Quality Validation',
     packages=find_packages(where='src'),
