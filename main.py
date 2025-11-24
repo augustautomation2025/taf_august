2dd4cbfce786969888cbe9e062464f37024730d0
admin <kattubadi426@gmail.com>
2025-11-24 07:51:09 +0530

    hi

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ print("this taf")
 print("this is taf line 2")
 
 print("this is taf line 2")
+
+# team1 - create new branch and add framework in it
+
+# team2 - create one feature branch and add test cases for table5
+
+#Fetch - downloads new code from remote for all branches
